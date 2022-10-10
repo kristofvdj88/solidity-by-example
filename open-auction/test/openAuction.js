af4a70ac0a6064ea850a83573c9bf07151a17939
Kristof Van Der Jonckheyd <kristof.van.der.jonckheyd@telenetgroup.be>
2022-10-10 19:29:56 +0200

    update readme's, add pretier config

---
+++ open-auction/test/openAuction.js
@@ -0,0 +1,7 @@
+var OpenAuction = artifacts.require("./OpenAuction.sol");
+
+contract("OpenAuction", function (accounts) {
+    const owner = accounts[0];
+    const alice = accounts[1];
+    const bob = accounts[2];
+});
